fb0d870292f22b06dc002d45dec0d10b958e8296
komer <hyluo.1999@qq.com>
2025-07-18 15:31:33 +0800

    add a simple ftxui example

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
 
-int main(int argc, char** argv) {
-    std::cout << "hello world!" << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+
+  using namespace ftxui;
+
+  Element document = hbox({
+      text("left") | border,
+      text("middle") | border | flex,
+      text("right") | border,
+  });
+
+  auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
+
+  Render(screen, document);
+
+  screen.Print();
+
+  return 0;
 }
